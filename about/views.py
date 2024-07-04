204f85d8e2aa1375324e7062966e105156a7f62b
betaru <betaru74@gmail.com>
2024-07-04 20:04:12 +0200

    Add dynamic current year to footer in home.html

--- about/views.py
+++ about/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def home(request):
-    return render(request, 'about/home.html')
+    current_year = datetime.now().year
+    return render(request, 'about/home.html', {'current_year': current_year})
