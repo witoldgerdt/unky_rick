c6221d94a238391b6468c41843bb736c73ea7862
betaru <betaru74@gmail.com>
2024-07-05 21:11:08 +0200

    test github

--- about/tests.py
+++ about/tests.py
@@ -1,11 +1,9 @@
-import os
 import pytest
-from django.test import TestCase
 from django.urls import reverse
 
-@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="Skipping DB tests in GitHub Actions")
-class AboutPageTest(TestCase):
-    def test_about_page_contains_expected_text(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Who is Unky Rick?")
+@pytest.mark.django_db
+class TestAboutPage:
+    def test_about_page_contains_expected_text(self, client):
+        response = client.get(reverse('home'))
+        assert response.status_code == 200
+        assert "Who is Unky Rick?" in response.content.decode()
