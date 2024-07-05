5a00afbfed39807f977878ac59b6f2f74f73b560
betaru <betaru74@gmail.com>
2024-07-05 18:53:24 +0200

    Add pytest and checking the about page

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
-
 # Create your tests here.
+# example_app/tests/test_views.py
+
+from django.test import TestCase
+from django.urls import reverse
+
+class AboutPageTest(TestCase):
+    def test_about_page_contains_expected_text(self):
+        response = self.client.get(reverse('home'))  #  URL name for test
+        self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, "expected text")  # 
