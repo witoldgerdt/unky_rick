9c3f8da13883d5a08f46b944878d97fcb2994051
betaru <betaru74@gmail.com>
2024-07-05 21:22:20 +0200

    Add special dir for test

---
+++ about/test/test_about.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+from django.urls import reverse
+
+@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') is None, reason="Skipping this test locally")
+@pytest.mark.django_db
+def test_about_page_contains_expected_text(client):
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
+    assert "Who is Unky Rick?" in response.content.decode()
