e81ba3cd9bec9af6809662f5f9bf519835adea4c
betaru <betaru74@gmail.com>
2024-07-12 21:34:50 +0200

    Fix 502 error and update configuration

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit on error
 
-# pip install --upgrade pip
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Start the server
-gunicorn unky_rick.wsgi:application --workers 3 --bind 0.0.0.0:$PORT &
+# gunicorn unky_rick.wsgi:application --workers 3 --bind 0.0.0.0:$PORT &
+gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --log-level debug
 
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
