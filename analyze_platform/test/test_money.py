855c0b82d04bc42fb4d6ab2805db7044963b7c5c
betaru <betaru74@gmail.com>
2024-07-12 20:14:01 +0200

    Fix test

--- analyze_platform/test/test_money.py
+++ analyze_platform/test/test_money.py
@@ -1,16 +1,14 @@
-import pytest
 from django.urls import reverse
-
-@pytest.mark.django_db
-def test_money_page_contains_expected_text(client):
-    response = client.get(reverse('money'))
-    assert response.status_code == 200
-    assert "Money Page" in response.content.decode()
+import pytest
 
 @pytest.mark.django_db
 def test_about_page_button_redirects_to_money(client):
     response = client.get(reverse('about'))
     assert response.status_code == 200
     assert "To cultivate a new mindset." in response.content.decode()
+
+@pytest.mark.django_db
+def test_money_page_contains_expected_text(client):
     response = client.get(reverse('money'))
     assert response.status_code == 200
+    assert "Money Page" in response.content.decode()
