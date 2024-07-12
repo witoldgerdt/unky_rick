0758ef4bf0f9ceee93cb3a66091f81da5e7f0e59
betaru <betaru74@gmail.com>
2024-07-12 21:48:09 +0200

    Add debug

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 # Start the server
 # gunicorn unky_rick.wsgi:application --workers 3 --bind 0.0.0.0:$PORT &
-gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --log-level debug
+# gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --log-level debug
+gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
 
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
