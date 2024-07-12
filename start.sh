bb03d8f8ba07fe9fc5ee9981ea68625055b7593f
betaru <betaru74@gmail.com>
2024-07-12 20:41:54 +0200

    Increase timeout

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT &
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
   echo "Waiting for the server to be ready..."
-  sleep 5
+  sleep 10
 done
 
 # Run tests
