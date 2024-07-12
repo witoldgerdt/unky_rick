f4edf28160785d2713ec7ce26d7eaedd9e2f2b5d
betaru <betaru74@gmail.com>
2024-07-12 20:00:15 +0200

    Add pip upgrade

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit on error
 
+pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
