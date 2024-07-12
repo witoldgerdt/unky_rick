92279a633162398c4149f2e1c624d1aa3b5b16bd
betaru <betaru74@gmail.com>
2024-07-12 22:31:27 +0200

    Remove trash debug

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Start the server
-# gunicorn unky_rick.wsgi:application --workers 3 --bind 0.0.0.0:$PORT &
-# gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --log-level debug
 gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
 
 # Wait for the server to be ready
