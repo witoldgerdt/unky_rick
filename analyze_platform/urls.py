2bb90ed3d3e416ca9c40f63dc3edc2c2570fb0ec
betaru <betaru74@gmail.com>
2024-07-12 20:05:59 +0200

    Fix health_check

--- analyze_platform/urls.py
+++ analyze_platform/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.http import JsonResponse
 from . import views
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('', views.about, name='about'), 
     path('money/', views.money, name='money'),
-    path('about/', views.about, name='health_check'),
     path('splash/', views.splash, name='splash'),
+    path('health_check/', health_check, name='health_check'),
 ]
