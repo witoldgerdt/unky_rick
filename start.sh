ca370688a35153b7d5b033503323bc2b2ac06741
betaru <betaru74@gmail.com>
2024-07-12 20:47:03 +0200

    Fix test

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ gunicorn unky_rick.wsgi:application --bind 0.0.0.0:$PORT &
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
   echo "Waiting for the server to be ready..."
-  sleep 10
+  sleep 5
 done
 
 # Run tests
