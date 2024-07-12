6fb74bbd3d092d5410f4dd5ec134dc55d7abed25
betaru <betaru74@gmail.com>
2024-07-12 20:58:32 +0200

    Fix debug

--- unky_rick/settings.py
+++ unky_rick/settings.py
@@ -96,13 +96,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/django/debug.log',
-        },
-    },
     'loggers': {
         'django': {
             'handlers': ['file'],
