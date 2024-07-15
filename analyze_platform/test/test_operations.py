20f442f5c2b76125ba11fcc54abe56f0c76fa98d
betaru <betaru74@gmail.com>
2024-07-15 20:14:52 +0200

    Add pip upgrade and TestModel

--- analyze_platform/test/test_operations.py
+++ analyze_platform/test/test_operations.py
@@ -2,6 +2,7 @@ import pytest
 from analyze_platform.operations import DBManager
 from django.db import models
 
+@pytest.mark.django_db
 class TestModel(models.Model):
     column1 = models.CharField(max_length=100)
     column2 = models.CharField(max_length=100)
@@ -13,16 +14,19 @@ class TestModel(models.Model):
 def db_manager():
     return DBManager(TestModel)
 
+@pytest.mark.django_db
 def test_add_record(db_manager):
     db_manager.add_record(column1="value1", column2="value2")
     assert TestModel.objects.count() == 1
 
+@pytest.mark.django_db
 def test_update_record(db_manager):
     db_manager.add_record(column1="value1", column2="value2")
     db_manager.update_record(1, column1="updated_value1")
     record = TestModel.objects.get(pk=1)
     assert record.column1 == "updated_value1"
 
+@pytest.mark.django_db
 def test_remove_record(db_manager):
     db_manager.add_record(column1="value1", column2="value2")
     db_manager.remove_record(1)
--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit on error
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
