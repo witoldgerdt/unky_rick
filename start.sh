44830a00f3fbb467dd02131d5d52d15a307d0a41
betaru <betaru74@gmail.com>
2024-07-15 18:18:41 +0200

    fix path to scheduler

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
 
 # Start the scheduler in the background
-python path/to/your_scheduler_script.py &
+python analyze_platform/scheduler.py &
 
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
