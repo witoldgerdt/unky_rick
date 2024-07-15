832945f970452ca64f072c1c27b0be37071c9e41
betaru <betaru74@gmail.com>
2024-07-15 18:51:23 +0200

    Move scheduler after health_checking

--- start.sh
+++ start.sh
@@ -3,15 +3,16 @@
 # Start the server
 gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
 
-# Start the scheduler in the background
-echo "Starting scheduler..."
-nohup python path/to/scheduler.py > scheduler.log 2>&1 &
 
 # Wait for the server to be ready
 until curl -s http://localhost:$PORT/health_check; do
   echo "Waiting for the server to be ready..."
   sleep 5
 done
+
+# Start the scheduler in the background
+echo "Starting scheduler..."
+nohup python path/to/scheduler.py &
 
 # Run tests
 pytest
--- unky_rick/settings.py
+++ unky_rick/settings.py
@@ -7,8 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
 SECRET_KEY = '5a4sd65as4d'  # TODO: Change this to an environment variable for production
-# DEBUG = 'False'
-DEBUG = 'True'
+DEBUG = 'False'
 ALLOWED_HOSTS = ['unky-rick.onrender.com', 'localhost', '127.0.0.1']
 
 
