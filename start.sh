6ae54b51a13a58b029cf572cfa8b10e3dd2691a1
betaru <betaru74@gmail.com>
2024-07-16 20:53:00 +0200

    Disable scheduler, return & for app

--- start.sh
+++ start.sh
@@ -11,20 +11,20 @@
 
 # Start the server using Gunicorn in the background
 echo "Starting applicatiion..."
-gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
+gunicorn unky_rick.wsgi:application --config gunicorn.conf.py &
 
 # Wait for the server to be ready
-# until curl -s http://localhost:$PORT/health_check; do
-#   echo "Waiting for the server to be ready..."
-#   sleep 5
-# done
+until curl -s http://localhost:$PORT/health_check; do
+  echo "Waiting for the server to be ready..."
+  sleep 5
+done
 
 # Reset the test database
 # python manage.py reset_test_db
 
 # Start the scheduler in the background
-echo "Starting scheduler..."
-python analyze_platform/scheduler.py
+# echo "Starting scheduler..."
+# python analyze_platform/scheduler.py
 
 # Run tests
-# pytest 
+pytest 
