5e1a53961625858528812b1460be06699d75f9b6
betaru <betaru74@gmail.com>
2024-07-16 19:37:50 +0200

    Add curl test and pytest

--- start.sh
+++ start.sh
@@ -13,10 +13,10 @@
 gunicorn unky_rick.wsgi:application --config gunicorn.conf.py 
 
 # Wait for the server to be ready
-# until curl -s http://localhost:$PORT/health_check; do
-#   echo "Waiting for the server to be ready..."
-#   sleep 5
-# done
+until curl -s http://localhost:$PORT/health_check; do
+  echo "Waiting for the server to be ready..."
+  sleep 5
+done
 
 # Reset the test database
 # python manage.py reset_test_db
@@ -26,4 +26,4 @@ gunicorn unky_rick.wsgi:application --config gunicorn.conf.py
 # nohup python analyze_platform/scheduler.py &
 
 # Run tests
-# pytest
+pytest
