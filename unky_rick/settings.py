6129eb3a39439faa7ab265a6d1b8da9ae4077ac1
betaru <betaru74@gmail.com>
2024-07-17 21:41:20 +0200

    Add test context

--- unky_rick/settings.py
+++ unky_rick/settings.py
@@ -50,6 +50,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'your_app.context_processors.is_testing',  # Add this line
             ],
         },
     },
