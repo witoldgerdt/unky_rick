830f4b77e9586803c27a54a81a4fc81d3984ad42
betaru <betaru74@gmail.com>
2024-07-17 18:06:18 +0200

    Remove others models

--- analyze_platform/models.py
+++ analyze_platform/models.py
@@ -1,24 +1,5 @@
 from django.db import models
 
-class BaseModel(models.Model):
-    """
-    Abstract base model to include common fields or methods
-    """
-    id = models.AutoField(primary_key=True)
-
-    class Meta:
-        abstract = True
-
-class Record(BaseModel):
-    """
-    Model representing a record with two columns.
-    """
-    column1 = models.CharField(max_length=50)
-    column2 = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"Record {self.id}: {self.column1}, {self.column2}"
-
 class DataRecord(models.Model):
     column1 = models.CharField(max_length=255)
     column2 = models.TextField()
