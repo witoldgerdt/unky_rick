d2a0cefd4113e08eaf8c86d2774150d4731f853d
betaru <betaru74@gmail.com>
2024-07-17 22:31:30 +0200

    Remove wrong link

--- analyze_platform/test/test_money.py
+++ analyze_platform/test/test_money.py
@@ -1,8 +1,8 @@
-from django.urls import reverse
 import pytest
+from django.urls import reverse
 
 @pytest.mark.django_db
 def test_money_page_contains_expected_text(client):
     response = client.get(reverse('money'))
     assert response.status_code == 200
-    assert "This is the money page." in response.content.decode()
+    assert "Money Page" in response.content.decode()
