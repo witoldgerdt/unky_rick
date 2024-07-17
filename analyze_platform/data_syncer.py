44ec27a204e29b2dde735db621d5e5587873a336
betaru <betaru74@gmail.com>
2024-07-17 21:54:18 +0200

    Add excption

--- analyze_platform/data_syncer.py
+++ analyze_platform/data_syncer.py
@@ -1,20 +1,24 @@
 import time
 from django.shortcuts import render
+from django.urls import resolve
 
 class DataSyncerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        return self.get_response(request)
+        response = self.get_response(request)
+        return response
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        # Add any logic you need before the view is processed
+        # Skip splash screen for specific views
+        if resolve(request.path_info).url_name in ['about', 'money']:
+            return None
+        
         try:
             # Simulate some processing logic
             time.sleep(2)  # Example delay, replace with actual logic
             return render(request, 'analyze_platform/splash.html')
         except Exception as e:
-            # Handle any exceptions and render the splash screen
             print(f"Exception in process_view: {e}")
-            return render(request, 'analyze_platform/splash.html')
+            raise
