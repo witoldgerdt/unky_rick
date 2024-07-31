0a5d3dfd33416a9e0691f956e3d8af1653c59140
betaru <betaru74@gmail.com>
2024-07-31 19:19:37 +0200

    Fix pipeline

--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -3,82 +3,45 @@ import { Construct } from 'constructs';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
-import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class PipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // Source stage
-    const sourceOutput = new codepipeline.Artifact();
-    const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: 'GitHub_Source',
-      owner: 'your-github-username',
-      repo: 'your-repo-name',
-      oauthToken: cdk.SecretValue.secretsManager('github-token'),
-      output: sourceOutput,
-      branch: 'main'
-    });
+        // Define the pipeline
+        const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+            pipelineName: 'MyPipeline',
+        });
 
-    // Build stage
-    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml')
-    });
-    const buildOutput = new codepipeline.Artifact('BuildOutput');
-    const buildAction = new codepipeline_actions.CodeBuildAction({
-      actionName: 'CodeBuild',
-      project: buildProject,
-      input: sourceOutput,
-      outputs: [buildOutput]
-    });
+        // Define the source stage
+        const sourceOutput = new codepipeline.Artifact();
+        pipeline.addStage({
+            stageName: 'Source',
+            actions: [
+                new codepipeline_actions.GitHubSourceAction({
+                    actionName: 'GitHub_Source',
+                    owner: 'OWNER',
+                    repo: 'REPO',
+                    branch: 'main',
+                    oauthToken: cdk.SecretValue.secretsManager('prod/GITHUB_TOKEN'),
+                    output: sourceOutput,
+                }),
+            ],
+        });
 
-    // Deploy stage (to prepare DB)
-    const dbDeployProject = new codebuild.PipelineProject(this, 'DBDeployProject', {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml')
-    });
-    const dbDeployOutput = new codepipeline.Artifact('DBDeployOutput');
-    const dbDeployAction = new codepipeline_actions.CodeBuildAction({
-      actionName: 'DBDeploy',
-      project: dbDeployProject,
-      input: sourceOutput,
-      outputs: [dbDeployOutput],
-      environmentVariables: {
-        'STAGE': {
-          value: 'prepare_db'
-        }
-      }
-    });
-
-    // Deploy stage (to deploy the app)
-    const deployAction = new codepipeline_actions.CodeDeployServerDeployAction({
-      actionName: 'CodeDeploy',
-      input: dbDeployOutput,
-      deploymentGroup: codedeploy.ServerDeploymentGroup.fromServerDeploymentGroupAttributes(this, 'CodeDeployGroup', {
-        application: codedeploy.ServerApplication.fromServerApplicationName(this, 'unky_rick_app', 'unky_rick_app'),
-        deploymentGroupName: 'unky_rick_deployment_group',
-      }),
-    });
-
-    // Define the pipeline
-    new codepipeline.Pipeline(this, 'Pipeline', {
-      stages: [
-        {
-          stageName: 'Source',
-          actions: [sourceAction]
-        },
-        {
-          stageName: 'Build',
-          actions: [buildAction]
-        },
-        {
-          stageName: 'DBDeploy',
-          actions: [dbDeployAction]
-        },
-        {
-          stageName: 'Deploy',
-          actions: [deployAction]
-        }
-      ]
-    });
-  }
+        // Define the build stage
+        const buildOutput = new codepipeline.Artifact();
+        pipeline.addStage({
+            stageName: 'Build',
+            actions: [
+                new codepipeline_actions.CodeBuildAction({
+                    actionName: 'CodeBuild',
+                    project: new codebuild.PipelineProject(this, 'BuildProject'),
+                    input: sourceOutput,
+                    outputs: [buildOutput],
+                }),
+            ],
+        });
+    }
 }
